a96bc7024328f1a720302f9f4f79a837a31dccd2
Joseph Ugowe <joe@spheares.com>
2019-08-01 14:56:47 -0400

    Update dependency

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -3,11 +3,11 @@ const redisClient = require('./signin').redisClient;
 const requireAuth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    return res.status(401).json('Unauthorized🇮🇸');
+    return res.status(401).json('Unauthorized');
   }
   return redisClient.get(authorization, (err, reply) => {
     if (err || !reply){
-      return res.status(401).json('Unauthorized❤️');
+      return res.status(401).json('Unauthorized');
     }
     console.log('You shall pass')
     return next();
